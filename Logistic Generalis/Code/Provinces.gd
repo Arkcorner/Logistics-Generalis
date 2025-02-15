1f80b949483eab1ab73e440dd46ec1ac19c06906
Jens Winkel <115979577+Arkcorner@users.noreply.github.com>
2025-02-15 15:59:34 +0100

    changed Magic

--- Logistic Generalis/Code/Provinces.gd
+++ Logistic Generalis/Code/Provinces.gd
@@ -1,10 +1,12 @@
 extends Node
-
+var x : int = 1
 
 func _ready():
 	# Iterate through all child nodes and find their neighbors
 	for child in get_children():
 		if child is Province:
+			x += 1
+			child.province_number = x
 			child.find_neighbors()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
